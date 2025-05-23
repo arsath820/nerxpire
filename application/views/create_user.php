6a280a7d4867f8facb1728df28f41e13dbb03c64
Axlin Jenes <axlinjenes07@gmail.com>
2025-05-23 13:40:26 +0530

    added a header to create page

--- application/views/create_user.php
+++ application/views/create_user.php
@@ -1,3 +1,4 @@
+<h2>create form</h2>
 <form method="post" action="<?= base_url('index.php/user/store') ?>">
     Name: <input type="text" name="name" required><br>
     Age: <input type="number" name="age" required><br>
